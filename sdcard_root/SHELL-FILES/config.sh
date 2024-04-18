fe5e9c87acef53f690da57d1277d87e898a9221b
Jimmy Dansbo <jimmy@dansbo.dk>
2024-04-18 06:47:17 +0200

    Updated to shell v1.4 released on Mar 28, 2024 The shell program seems to have trouble when running on an OS or filesystem that is case sensitive. It works perfectly fine when run from a FAT32 filesystem or a Windows machine.

---
+++ sdcard_root/SHELL-FILES/config.sh
@@ -0,0 +1,26 @@
+# Shell startup and configuration script.
+# note: character encoding is ISO!
+# note2: keep the size under 1 kilobyte otherwise it overwrites basic program memory!
+
+# Screen mode and colors. Comment out to keep the defaults.
+mode 1
+color 1,11,11
+hicolor 14,13,10
+
+# Aliases
+alias dir=ls
+alias type=cat
+alias del=rm
+alias ren=mv
+alias copy=cp
+alias vi=nano
+alias pico=nano
+alias edit=nano
+alias date=time
+
+echo
+echo "  Commander-X16 Shell v1.4 - https://github.com/irmen/cx16shell"
+echo
+
+# Display the Message of the Day.
+cat //shell-files/:motd.txt
